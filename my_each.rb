922dec66d2dc36cb0dd6077431d104b5f5d04ded
john beltrez <beltrezjohn@gmail.com>
2020-07-16 20:57:45 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,7 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   counter = 0 
   while i < array.length 
   yield array[i]
   counter += 1# code here
 end
 
-def my_each(array)
-  
-end
